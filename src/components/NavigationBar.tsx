ae71a6308da81289ffdd18d6b49d75cd4c564a5b
bhavesh19354 <85835438+bhavesh19354@users.noreply.github.com>
2025-06-15 13:57:44 +0530

    Create NavigationBar.tsx

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const NavigationBar = () => {
+  return (
+    <nav className="bg-white border-b border-gray-200">
+    </nav>
+  );
+};
+
+export default NavigationBar;
